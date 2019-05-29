59b11fa658220be02a66e9f3574acdf3bbafe75c
Peerapong Jaipakdee <pjaipakdee@celestica.com>
2019-05-29 19:53:22 +0700

    Update install script to prevent installer to delete other OS.

--- packages/base/all/vendor-config-onl/src/python/onl/install/BaseInstall.py
+++ packages/base/all/vendor-config-onl/src/python/onl/install/BaseInstall.py
@@ -228,9 +228,20 @@ class Base:
                 nextBlock = max(part.geometry.start+part.geometry.length,
                                 nextBlock)
             else:
-                self.log.info("deleting this part")
-                self.partedDisk.removePartition(part)
-                dirty = True
+                disk = "{}{}".format(self.partedDisk.device.path,part.number)
+                out = subprocess.Popen(['blkid',disk],stdout=subprocess.PIPE, 
+                stderr=subprocess.STDOUT)
+
+                stdout,stderr = out.communicate()
+
+                if stderr == None:
+                    if "ONL" not in stdout:
+                        self.log.info("CLS Diag OS custom: this patition is not ONL don't delete.")
+                        self.log.info("skip this part" )
+                    else:
+                        self.log.info("deleting this part")
+                        self.partedDisk.removePartition(part)
+                        dirty = True
 
         if dirty:
             self.partedDisk.commit()
