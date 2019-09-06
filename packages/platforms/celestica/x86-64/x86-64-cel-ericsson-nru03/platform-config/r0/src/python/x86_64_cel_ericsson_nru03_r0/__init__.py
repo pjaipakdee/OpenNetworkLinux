57533b54432727c63d2f901b60a76b47b1940e7e
Johnsonluojianfeng <luo_jianfeng@hotmail.com>
2019-09-06 22:19:12 +0800

    merge_from_module_tar_package

---
+++ packages/platforms/celestica/x86-64/x86-64-cel-ericsson-nru03/platform-config/r0/src/python/x86_64_cel_ericsson_nru03_r0/__init__.py
@@ -0,0 +1,8 @@
+from onl.platform.base import *
+from onl.platform.celestica import *
+
+class OnlPlatform_x86_64_cel_ericsson_nru03_r0(OnlPlatformCelestica,
+                                            OnlPlatformPortConfig_48x10_6x40):
+    PLATFORM='x86-64-cel-ericsson-nru03-r0'
+    MODEL="ericsson-nru03"
+    SYS_OBJECT_ID=".2060.1"
