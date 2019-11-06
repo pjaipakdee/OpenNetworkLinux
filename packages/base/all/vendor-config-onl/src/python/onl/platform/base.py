4e6104974fcfd8db65c6ec52afb3d4e1b7914b60
PJ <pjaipakdee@celestica.com>
2019-11-06 10:30:24 +0700

    Add platform config for support Silverstone2

--- packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
+++ packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
@@ -528,3 +528,7 @@ class OnlPlatformPortConfig_8x1_8x10(object):
 class OnlPlatformPortConfig_48x10_6x100(object):
     PORT_COUNT=54
     PORT_CONFIG="48x10 + 6x100"
+
+class OnlPlatformPortConfig_2x10_32x400(object):
+    PORT_COUNT=34
+    PORT_CONFIG="2x10 + 32x400"
