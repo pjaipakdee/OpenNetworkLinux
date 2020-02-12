e938003adc86b69178fbebc6ccdc3a0272e9904d
Peerapong <pjaipakdee@celestica.com>
2020-02-12 14:26:46 +0700

    Add Cloverstone-DP platform v1.0.0.

---
+++ packages/platforms/celestica/x86-64/x86-64-cel-cloverstone-dp/platform-config/r0/src/python/x86_64_cel_cloverstone_dp_r0/__init__.py
@@ -0,0 +1,8 @@
+from onl.platform.base import *
+from onl.platform.celestica import *
+
+class OnlPlatform_x86_64_cel_cloverstone_dp_r0(OnlPlatformCelestica,
+                                            OnlPlatformPortConfig_48x10_6x40):
+    PLATFORM='x86-64-cel-cloverstone-dp-r0'
+    MODEL="cloverstone-dp"
+    SYS_OBJECT_ID=".2060.1"
